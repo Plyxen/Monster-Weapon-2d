158b83ec6c4bd6b02dc0c536d4cf77bdd79e69d8
Plyxen <kucsak.akos@gmail.com>
2025-11-03 15:30:41 +0100

    Refactor enums.py to include docstrings for clarity on CellType, Direction, ItemType, and MonsterType classes

--- enums.py
+++ enums.py
@@ -1,9 +1,10 @@
 """Game enumerations for Monster-Weapon-2d"""
 
-from enum import Enum
+from enum import Enum, auto
 
 
 class CellType(Enum):
+    """Dungeon cell types."""
     WALL = "wall"
     PATH = "path"
     START = "start"
@@ -14,7 +15,27 @@ class CellType(Enum):
 
 
 class Direction(Enum):
+    """Cardinal directions."""
     NORTH = "north"
     SOUTH = "south"
     EAST = "east"
     WEST = "west"
+
+
+class ItemType(Enum):
+    """Types of collectible items."""
+    TREASURE = auto()
+    HEALTH_POTION = auto()
+    KEY = auto()
+    SWORD = auto()
+    SHIELD = auto()
+
+
+class MonsterType(Enum):
+    """Types of enemy monsters."""
+    FLY = auto()
+    GAPER = auto()
+    SHOOTER = auto()
+    TANK = auto()
+    SPEEDY = auto()
+    CHARGER = auto()
